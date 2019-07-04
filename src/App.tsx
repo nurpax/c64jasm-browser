4f4ba31a22550a84a076d42a0fe90098c40930ce
Janne Hellsten <jjhellst@gmail.com>
2019-07-04 22:08:26 +0300

    Diagnostics and line numbered gutter
    Also make tab work in the <textarea>

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,36 @@
 import React, { Fragment } from 'react';
 
-import { assembleWithOptions, disassemble } from 'c64jasm'
+import { SourceLoc, assembleWithOptions, disassemble } from 'c64jasm';
 
-import Editor from './Editor'
+import Editor from './Editor';
 import Disasm from './Disasm';
+
+import styles from './App.module.css';
+
+class Diagnostic extends React.Component<{ error: { loc: SourceLoc, msg: string } }> {
+  render () {
+    const { loc, msg }  = this.props.error;
+    return (
+      <div>
+        main.asm:{loc.start.line}:{loc.start.column}: error: {msg}
+      </div>
+    )
+  }
+}
+
+type Diag = { loc: SourceLoc, msg: string, formatted: string };
 
 interface AppState {
   sourceCode: string;
   disassembly: string[];
+  diagnostics: Diag[];
 };
 
 class App extends React.Component<{}, AppState> {
   state = {
     sourceCode: '',
-    disassembly: []
+    disassembly: [],
+    diagnostics: []
   }
 
   handleSetSource = (text: string) => {
@@ -22,20 +39,32 @@ class App extends React.Component<{}, AppState> {
       }
       const res = assembleWithOptions("foo.asm", options);
       if (res.errors.length === 0) {
-        this.setState({disassembly: disassemble(res.prg)});
+        this.setState({
+          disassembly: disassemble(res.prg),
+          diagnostics: []
+        });
+      } else {
+        this.setState({
+          diagnostics: res.errors
+        })
       }
     }
 
   render () {
+    const diags = this.state.diagnostics;
     return (
       <Fragment>
         <header id="pageHeader">Header</header>
         <div id="mainCode">
-          <Editor onSourceChanged={this.handleSetSource} />
+          <Editor onSourceChanged={this.handleSetSource} diagnostics={this.state.diagnostics} />
         </div>
-        <nav id="mainNav">Nav</nav>
         <div id="siteDisasm">
           <Disasm disassembly={this.state.disassembly} />
+        </div>
+        <div id="mainDiag">
+          <div className={styles.diagnostics}>
+            {diags.map((d: Diag) => <Diagnostic key={JSON.stringify(d.loc)} error={d} />)}
+          </div>
         </div>
         <footer id="pageFooter">Footer</footer>
       </Fragment>
--- src/Editor.tsx
+++ src/Editor.tsx
@@ -1,15 +1,53 @@
 
 import React from 'react';
+import indentTextarea from 'indent-textarea';
 
+import { SourceLoc } from 'c64jasm'
 import styles from './Editor.module.css'
+
+const Gutter = React.forwardRef((props: {}, ref: React.Ref<HTMLDivElement>) => {
+  const rows = [];
+  for (let i = 0; i < 100; i++) {
+    const str = `${i+1}`;
+    rows.push(<div className={styles.gutterRow} key={i}>{str.padStart(4, ' ')}</div>);
+  }
+  return (
+    <div ref={ref} className={styles.gutter}>
+      {rows}
+    </div>
+  );
+});
 
 interface EditorProps {
   onSourceChanged: (text: string) => void;
+  diagnostics: { loc: SourceLoc, msg: string }[];
 }
 
-export default class extends React.Component<EditorProps> {
+interface EditorState {
+  scrollTop: number;
+}
+
+export default class extends React.Component<EditorProps, EditorState> {
+  textareaRef = React.createRef<HTMLTextAreaElement>();
+  gutterRef = React.createRef<HTMLDivElement>();
+
+  handleScroll = (e: React.UIEvent<HTMLTextAreaElement>) => {
+    const { scrollTop } = e.nativeEvent.target as any;
+    if (this.gutterRef && this.gutterRef.current) {
+      this.gutterRef.current.scrollTop = scrollTop;
+    }
+    this.setState({ scrollTop })
+  }
+
   handleSourceChanged = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
     this.props.onSourceChanged(e.target.value);
+  }
+
+  componentDidMount () {
+    if (this.textareaRef && this.textareaRef.current) {
+      indentTextarea.watch(this.textareaRef.current);
+      this.textareaRef.current.spellcheck = false;
+    }
   }
 
   render () {
@@ -17,7 +55,11 @@ export default class extends React.Component<EditorProps> {
       <div className={styles.layoutContainer}>
         <div className={styles.heading}>Assembly</div>
         <div className={styles.editorContainer}>
-          <textarea onChange={this.handleSourceChanged} className={styles.textarea}></textarea>
+          <Gutter ref={this.gutterRef} />
+          <textarea
+            onScroll={this.handleScroll}
+            ref={this.textareaRef}
+            onChange={this.handleSourceChanged} className={styles.textarea}></textarea>
         </div>
       </div>
     )
