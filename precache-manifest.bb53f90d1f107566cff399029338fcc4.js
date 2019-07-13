b96d398df8a16246d9a02880270d54b8ece6108d
Janne Hellsten <jjhellst@gmail.com>
2019-07-13 22:00:50 +0300

    Updates

---
+++ precache-manifest.bb53f90d1f107566cff399029338fcc4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4492c3b7c904795af75ea3bd8b286cf9",
+    "url": "/c64jasm-browser/index.html"
+  },
+  {
+    "revision": "8d352ca7fcde852e218f",
+    "url": "/c64jasm-browser/static/css/main.ada58b06.chunk.css"
+  },
+  {
+    "revision": "18136ddc4a3c5dd9a918",
+    "url": "/c64jasm-browser/static/js/2.e13d6693.chunk.js"
+  },
+  {
+    "revision": "8d352ca7fcde852e218f",
+    "url": "/c64jasm-browser/static/js/main.a1597aef.chunk.js"
+  },
+  {
+    "revision": "b47b758841457febcc13",
+    "url": "/c64jasm-browser/static/js/runtime~main.9ccd4be7.js"
+  }
+]);
