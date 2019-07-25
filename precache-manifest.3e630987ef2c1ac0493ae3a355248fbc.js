7662eefbb79bac8d29abab140e30bf224a07056f
Janne Hellsten <jjhellst@gmail.com>
2019-07-26 01:36:33 +0300

    Updates

---
+++ precache-manifest.3e630987ef2c1ac0493ae3a355248fbc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1bc5c674fd2d8af33f9dde27a42692cf",
+    "url": "/c64jasm-browser/index.html"
+  },
+  {
+    "revision": "0a7de9054ef0edd1e9b4",
+    "url": "/c64jasm-browser/static/css/main.76a5e824.chunk.css"
+  },
+  {
+    "revision": "99f3f8024ac39beea8cf",
+    "url": "/c64jasm-browser/static/js/2.6adb430b.chunk.js"
+  },
+  {
+    "revision": "0a7de9054ef0edd1e9b4",
+    "url": "/c64jasm-browser/static/js/main.378cf6e1.chunk.js"
+  },
+  {
+    "revision": "3f9b788defdd9a54a6c1",
+    "url": "/c64jasm-browser/static/js/runtime~main.d6512d46.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/c64jasm-browser/precache-manifest.ca885a0ced4c1872b7b6a80462429e6d.js"
+  "/c64jasm-browser/precache-manifest.3e630987ef2c1ac0493ae3a355248fbc.js"
 );
 
 self.addEventListener('message', (event) => {
